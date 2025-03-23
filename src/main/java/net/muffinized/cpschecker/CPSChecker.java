151aaf673955c80099574149be7ca2b81a5b7a22
Larssies <larsvanwylick@gmail.com>
2025-03-23 15:04:44 +0100

    Initial commit

---
+++ src/main/java/net/muffinized/cpschecker/CPSChecker.java
@@ -0,0 +1,60 @@
+package net.muffinized.cpschecker;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public final class CPSChecker extends JavaPlugin implements Listener {
+
+    private final HashMap<UUID, Integer> clickCounts = new HashMap<>();
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        startCPSResetTask();
+    }
+
+    @Override
+    public void onDisable() {
+        // do whatever you want here
+    }
+
+    @EventHandler
+    public void onPlayerClick(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+        int MAX_CPS = 20;
+
+        switch (event.getAction()) {
+            case RIGHT_CLICK_AIR:
+            case RIGHT_CLICK_BLOCK:
+            case LEFT_CLICK_AIR:
+            case LEFT_CLICK_BLOCK:
+                UUID playerId = player.getUniqueId();
+                clickCounts.put(playerId, clickCounts.getOrDefault(playerId, 0) + 1);
+
+                if (clickCounts.get(playerId) > MAX_CPS) {
+                    player.sendMessage(ChatColor.RED + "⚠ Warning: You are clicking over 20 CPS!");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void startCPSResetTask() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                clickCounts.clear();
+            }
+        }.runTaskTimer(this, 20, 20);
+    }
+}
